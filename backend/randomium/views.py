f872f9bcc107cec7b3f0099affff8e2eb2ae5f70
Taras Savchyn <savik1992@gmail.com>
2024-04-02 22:17:33 +0300

    add Bank filtering by city & country

--- backend/randomium/views.py
+++ backend/randomium/views.py
@@ -8,6 +8,23 @@ class BankViewSet(viewsets.ModelViewSet):
     queryset = Bank.objects.all()
     serializer_class = BankSerializer
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        city = self.request.query_params.get("city", None)
+        country = self.request.query_params.get("country", None)
+
+        if city and country:
+            # Filter banks based on city and country
+            queryset = queryset.filter(address__city=city, address__country=country)
+        elif city:
+            # Filter banks based on city only
+            queryset = queryset.filter(address__city=city)
+        elif country:
+            # Filter banks based on country only
+            queryset = queryset.filter(address__country=country)
+
+        return queryset
+
 
 class AddressViewSet(viewsets.ModelViewSet):
     queryset = Address.objects.all()
