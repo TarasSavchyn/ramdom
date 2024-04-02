1be0765fc0eacc5a38cbff4879b5cbb858275c97
Taras Savchyn <savik1992@gmail.com>
2024-04-02 21:43:21 +0300

    create models Bank & Address

--- backend/randomium/models.py
+++ backend/randomium/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+RATINGS_CHOICES = [("good", "Good"), ("average", "Average"), ("poor", "Poor")]
+
+
+class Bank(models.Model):
+    name = models.CharField(max_length=100)
+    rating = models.CharField(max_length=63, choices=RATINGS_CHOICES)
+    address = models.ForeignKey(
+        "Address", on_delete=models.CASCADE, related_name="banks"
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="banks", null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.country}"
