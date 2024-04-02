892d521eb47e690635c1541eb1ac26fe9b13990e
Taras Savchyn <savik1992@gmail.com>
2024-04-02 21:57:41 +0300

    add Bank/Adress ViewSet

--- backend/randomium/views.py
+++ backend/randomium/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from randomium.models import Bank, Address
+from randomium.serializers import BankSerializer, AddressSerializer
+
+
+class BankViewSet(viewsets.ModelViewSet):
+    queryset = Bank.objects.all()
+    serializer_class = BankSerializer
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
